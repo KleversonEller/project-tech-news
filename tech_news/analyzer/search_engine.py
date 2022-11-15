6a1ff18ea17692e44ddaf029d97cfba0d01f650b
Kleverson Eller <kleverson.a16@gmail.com>
2022-11-15 03:07:00 -0300

    feat: :sparkles: Implementando funçoes (search_engine.py)
    Implementando as funçoes (search_by_title, search_by_date, search_by_tag, search_by_category)

--- tech_news/analyzer/search_engine.py
+++ tech_news/analyzer/search_engine.py
@@ -1,18 +1,38 @@
 # Requisito 6
+import datetime
+from tech_news.database import search_news
+
+
 def search_by_title(title):
     """Seu código deve vir aqui"""
+    news = search_news({"title": {"$regex": title, "$options": "i"}})
+    return [(new["title"], new["url"]) for new in news]
 
 
 # Requisito 7
 def search_by_date(date):
     """Seu código deve vir aqui"""
+    try:
+        format_date = datetime.datetime.strptime(date, "%Y-%m-%d").strftime(
+            "%d/%m/%Y"
+        )
+        news = search_news({"timestamp": {"$regex": format_date}})
+        return [(new["title"], new["url"]) for new in news]
+    except ValueError:
+        raise ValueError("Data inválida")
 
 
 # Requisito 8
 def search_by_tag(tag):
     """Seu código deve vir aqui"""
+    news = search_news(
+        {"tags": {"$elemMatch": {"$regex": tag, "$options": "i"}}}
+    )
+    return [(new["title"], new["url"]) for new in news]
 
 
 # Requisito 9
 def search_by_category(category):
     """Seu código deve vir aqui"""
+    news = search_news({"category": {"$regex": category, "$options": "i"}})
+    return [(new["title"], new["url"]) for new in news]
